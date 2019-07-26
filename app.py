3f58582b99c81c39ef4d2415640efe2181ac5171
babaraza <syedrazatx@gmail.com>
2019-07-26 16:27:44 -0500

    Auto delete temp images created

--- app.py
+++ app.py
@@ -15,28 +15,37 @@ from pathlib import Path
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 source_dir = Path()
-source_files = source_dir.glob('*.pdf')  # Search for all pdf files
+
+
+def get_files():
+    source_files = source_dir.glob('*.pdf')  # Search for all pdf files
+    # Go through each pdf file and convert to JPG
+    for file in source_files:
+        with Image(filename=str(file), resolution=200) as img:
+            print("Converting PDF to JPG")
+            img.compression_quality = 99
+            img.format = 'jpg'
+            # file stem is name of file without ext
+            img.save(filename="{}.{}".format(file.stem, img.format))
+            ocr_file(file.stem)
 
 
 # Go through each JPG file, perform OCR on it, save as text file
-def ocr_file(origfile):
-    print("Applying OCR on PDF")
-    for image in source_dir.glob(f'{origfile}*.jpg'):
+def ocr_file(orig_filename):
+    print("Applying OCR on JPG (s)")
+    for image in source_dir.glob(f'{orig_filename}*.jpg'):
         text = pytesseract.image_to_string(PIL_Image.open(image))
-        filename = '{}.txt'.format(origfile)
+        filename = '{}.txt'.format(orig_filename)
         # 'a' mode allows us to append to text file
-        with open(filename, 'a') as txt:
-            txt.write(text)
+        with open(filename, "a") as txt:
+            txt.write(f'{text}\n\n')
+    if input("Delete JPG(s)? > ").lower() == "y":
+        print("Deleting Temporary Images")
+        for temp_img in source_dir.glob('*.jpg'):
+            temp_img.unlink()  # Delete jpg(s) that were created
+    else:
+        print("Ending Program")
 
 
-# Go through each pdf file and convert to JPG
-for file in source_files:
-    with Image(filename=str(file), resolution=200) as pdf:
-        print("Converting PDF to JPG")
-        pdf.compression_quality = 99
-        pdf.format = 'jpg'
-        # file stem is name of file without ext
-        pdf.save(filename="{}.{}".format(file.stem, pdf.format))
-        ocr_file(file.stem)
-
+get_files()
 print("Done")
