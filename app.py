03047c389e45d0288f6ccd11721441d729c96422
babaraza <syedrazatx@gmail.com>
2019-07-26 15:31:43 -0500

    Testing Memory Usage

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from wand.image import Image
 from PIL import Image as PIL_Image
 import pytesseract
 from pathlib import Path
+import numpy as np
+import cv2
 
 # Requirements:
 # Tesseract from https://github.com/UB-Mannheim/tesseract/wiki
@@ -10,6 +12,8 @@ from pathlib import Path
 # pip install wand
 # pip install pillow
 # pip install pytesseract
+# pip install numpy
+# pip install opencv-python
 
 # Show where Tesseract is installed for pytesseract to work
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -17,26 +21,21 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 source_dir = Path()
 source_files = source_dir.glob('*.pdf')  # Search for all pdf files
 
+# to avoid creating and deleting a temporary file for each image we use numpy and OpenCV
+# to extract the image as a blob, convert it to a numpy array and then turn it into a PIL image
+# for pytesseract to perform OCR
 
-# Go through each JPG file, perform OCR on it, save as text file
-def ocr_file(origfile):
-    print("Applying OCR on PDF")
-    for image in source_dir.glob(f'{origfile}*.jpg'):
-        text = pytesseract.image_to_string(PIL_Image.open(image))
-        filename = '{}.txt'.format(origfile)
-        # 'a' mode allows us to append to text file
+# TODO: Get Page 2 to work
+
+for file in source_files:
+    with Image(filename=str(file), resolution=200) as img:
+        img.format = 'jpg'
+        img.compression_quality = 99
+        img_buffer = np.asarray(bytearray(img.make_blob()))
+        img_data = cv2.imdecode(img_buffer, cv2.IMREAD_GRAYSCALE)
+        text = pytesseract.image_to_string(PIL_Image.fromarray(img_data))
+        filename = '{}.txt'.format(file.stem)
         with open(filename, 'a') as txt:
             txt.write(text)
-
-
-# Go through each pdf file and convert to JPG
-for file in source_files:
-    with Image(filename=str(file), resolution=200) as pdf:
-        print("Converting PDF to JPG")
-        pdf.compression_quality = 99
-        pdf.format = 'jpg'
-        # file stem is name of file without ext
-        pdf.save(filename="{}.{}".format(file.stem, pdf.format))
-        ocr_file(file.stem)
 
 print("Done")
