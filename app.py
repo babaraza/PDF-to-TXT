e91b7eeb9d3e62520226d43cd3973706962d5280
babaraza <syedrazatx@gmail.com>
2019-08-01 10:34:24 -0500

    Clean up

--- app.py
+++ app.py
@@ -19,15 +19,19 @@ source_dir = Path()
 
 def get_files():
     source_files = source_dir.glob('*.pdf')  # Search for all pdf files
+
     # Go through each pdf file and convert to JPG
     for file in source_files:
         with Image(filename=str(file), resolution=200) as img:
             print("Converting PDF to JPG")
             img.compression_quality = 99
             img.format = 'jpg'
+
+            # TODO: create a temporary folder to store these files
             # Saving jpg with the same name as the source pdf file
             # File stem is name of file without extension
             img.save(filename="{}.{}".format(file.stem, img.format))
+
             # Sending filename of the pdf file to next function
             # Since images will have the same name
             ocr_file(file.stem)
@@ -36,17 +40,22 @@ def get_files():
 # Go through each JPG file, perform OCR on it, save as text file
 def ocr_file(orig_filename):
     print("Applying OCR on JPG (s)")
+
     # Using source pdf file name to search for same named images
     for image in source_dir.glob(f'{orig_filename}*.jpg'):
+
         # Using Wand to open jpg, casting into Pillow
         # Since pytesseract accepts PIL images
         text = pytesseract.image_to_string(PIL_Image.open(image))
+
         # Creating text file with the same name as source pdf
         filename = '{}.txt'.format(orig_filename)
+
         # 'a' mode allows us to append to text file
         with open(filename, "a") as txt:
             txt.write(f'{text}\n\n')
-    # Asking to delete temporarily created JPG(s)
+
+    # Ask to delete temporarily created JPG(s)
     if input("Delete JPG(s)? > ").lower() == "y":
         print("Deleting Temporary Images")
         for temp_img in source_dir.glob('*.jpg'):
