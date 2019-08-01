03317c30f2b68c90a3470c93ad6e2395418c8d92
babaraza <syedrazatx@gmail.com>
2019-08-01 13:08:07 -0500

    Implemented Temporary Folder

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from wand.image import Image
 from PIL import Image as PIL_Image
 import pytesseract
 from pathlib import Path
+import shutil
 
 # Requirements:
 # Tesseract from https://github.com/UB-Mannheim/tesseract/wiki
@@ -15,22 +16,29 @@ from pathlib import Path
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 source_dir = Path()
+temp_dir = Path('temp/')
 
 
 def get_files():
+    try:
+        temp_dir.mkdir()
+        print("Creating temporary folder")
+    except:
+        print("Received an Error when trying to create Temporary Folder")
+
+    print("Finding all PDF files")
     source_files = source_dir.glob('*.pdf')  # Search for all pdf files
 
     # Go through each pdf file and convert to JPG
     for file in source_files:
         with Image(filename=str(file), resolution=200) as img:
-            print("Converting PDF to JPG")
+            print("Converting PDF(s) to JPG(s)")
             img.compression_quality = 99
             img.format = 'jpg'
 
-            # TODO: create a temporary folder to store these files
             # Saving jpg with the same name as the source pdf file
             # File stem is name of file without extension
-            img.save(filename="{}.{}".format(file.stem, img.format))
+            img.save(filename="{}/{}.{}".format(temp_dir, file.stem, img.format))
 
             # Sending filename of the pdf file to next function
             # Since images will have the same name
@@ -39,27 +47,27 @@ def get_files():
 
 # Go through each JPG file, perform OCR on it, save as text file
 def ocr_file(orig_filename):
-    print("Applying OCR on JPG (s)")
+    print("Applying OCR on JPG(s)")
 
     # Using source pdf file name to search for same named images
-    for image in source_dir.glob(f'{orig_filename}*.jpg'):
+    for image in temp_dir.glob(f'{orig_filename}*.jpg'):
 
         # Using Wand to open jpg, casting into Pillow
         # Since pytesseract accepts PIL images
         text = pytesseract.image_to_string(PIL_Image.open(image))
 
         # Creating text file with the same name as source pdf
-        filename = '{}.txt'.format(orig_filename)
+        filename = '{}/{}.txt'.format(source_dir, orig_filename)
 
+        print("Creating text file with OCR data")
         # 'a' mode allows us to append to text file
         with open(filename, "a") as txt:
             txt.write(f'{text}\n\n')
 
-    # Ask to delete temporarily created JPG(s)
+    # Ask to delete Temp folder with JPG(s)
     if input("Delete JPG(s)? > ").lower() == "y":
-        print("Deleting Temporary Images")
-        for temp_img in source_dir.glob('*.jpg'):
-            temp_img.unlink()  # Delete jpg(s) that were created
+        print("Deleting Temporary Folder")
+        shutil.rmtree(temp_dir, ignore_errors=True)
     else:
         print("Ending Program")
 
